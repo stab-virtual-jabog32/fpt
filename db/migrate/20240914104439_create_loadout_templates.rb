b4550fff51eba7baeaf749e0c32bbcc122dee7df
Thomas Daubner <Daubner.Thomas@t-online.de>
2024-09-14 12:46:16 +0200

    defined migration

--- db/migrate/20240914104439_create_loadout_templates.rb
+++ db/migrate/20240914104439_create_loadout_templates.rb
@@ -1,6 +1,13 @@
-class CreateLoadoutTemplates < ActiveRecord::Migration[6.0]
+class CreateLoadoutTemplates < ActiveRecord::Migration[6.1]
   def change
     create_table :loadout_templates do |t|
+      t.string :name, null: false                # Name of the template
+      t.string :airframe, null: false            # Airframe the template is associated with
+      t.text :loadout, null: false               # The loadout data as text or JSON
+      t.references :user, foreign_key: true      # Optional: if you want to associate templates with users
+      t.timestamps
     end
+
+    add_index :loadout_templates, [:name, :airframe], unique: true # Ensure unique template names per airframe
   end
 end
