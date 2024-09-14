123daebdd457db8ea31701fd6ff45ddafdb0e7ac
Thomas Daubner <Daubner.Thomas@t-online.de>
2024-09-14 12:52:56 +0200

    successful migration

--- db/migrate/20240914104439_create_loadout_templates.rb
+++ db/migrate/20240914104439_create_loadout_templates.rb
@@ -1,4 +1,4 @@
-class CreateLoadoutTemplates < ActiveRecord::Migration[6.1]
+class CreateLoadoutTemplates < ActiveRecord::Migration[6.0]
   def change
     create_table :loadout_templates do |t|
       t.string :name, null: false                # Name of the template
