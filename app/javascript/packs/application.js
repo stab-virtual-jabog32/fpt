5813d5c8cabfff191d8f85c1ccae8572b9b01374
Thomas Daubner <Daubner.Thomas@t-online.de>
2024-10-08 14:19:59 +0200

    basic version added. Backend ready, Frontend as example only for somebody else

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require('../index_map.js')  // Make sure this points to the correct location
+
 
 import 'expose-loader?$!jquery';
 import 'bootstrap';
