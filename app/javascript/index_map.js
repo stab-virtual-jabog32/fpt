f9bc23bef64c984265cde6589ca929e7d62cdfce
Thomas Daubner <Daubner.Thomas@t-online.de>
2024-10-09 16:42:57 +0200

    tiles corrected

--- app/javascript/index_map.js
+++ app/javascript/index_map.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Initialize the map for each date
     var map = L.map(`map_${index}`);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 17,
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (CC-BY-SA)'
     }).addTo(map);
 
     // Add a legend for this map
