f2bbe3897e1929e22e37a7846fc3e7db2449966e
Thomas Daubner <Daubner.Thomas@t-online.de>
2025-01-10 17:54:06 +0100

    lock updated from production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-const webpack = require('webpack')
+// Add Node.js polyfills for Webpack 5
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+environment.plugins.append('NodePolyfillPlugin', new NodePolyfillPlugin());
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}));
 
-module.exports = environment
+module.exports = environment;
+
